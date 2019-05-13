56ab840e3e28018af85eb1ee835493c2a7c37138
matt-beaney <matt.beaney@autotrader.co.uk>
2019-05-13 15:29:13 +0100

    linting & godoc fixes

--- checkerr.go
+++ checkerr.go
@@ -1,30 +1,28 @@
-// package providing error handlers I use in pretty much every app I write
+// Package checkerr provides error handlers I use in pretty much every app I write
 package checkerr
 
 import "log"
 
-// check if an error is an error and log the supplied strings if so
+// Check if an error is an error and log the supplied strings if so
 func Check(e error, s ...string) bool {
-    if e != nil {
-        for _, str := range s {
-            log.Println(str)
-        }
-        log.Println(e.Error())
-        return true
-    } else {
-        return false
-    }
+	if e != nil {
+		for _, str := range s {
+			log.Println(str)
+		}
+		log.Println(e.Error())
+		return true
+	}
+	return false
 }
 
-// check if an error is an error, log the supplied strings and exit if so
+// CheckFatal checks if an error is an error, log the supplied strings and exit if so
 func CheckFatal(e error, s ...string) bool {
-    if e != nil {
-        for _, str := range s {
-            log.Println(str)
-        }
-        log.Fatalln(e.Error())
-        return true
-    } else {
-        return false
-    }
+	if e != nil {
+		for _, str := range s {
+			log.Println(str)
+		}
+		log.Fatalln(e.Error())
+		return true
+	}
+	return false
 }
