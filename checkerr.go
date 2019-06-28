39c3997c90d852956c12dff4d8c8e161ec272ab5
matt-beaney <matt.beaney@autotrader.co.uk>
2019-06-28 09:35:30 +0100

    add handlers for 401 & 500 errors

--- checkerr.go
+++ checkerr.go
@@ -1,7 +1,10 @@
 // Package checkerr provides error handlers I use in pretty much every app I write
 package checkerr
 
-import "log"
+import (
+	"log"
+	"net/http"
+)
 
 // Check if an error is an error and log the supplied strings if so
 func Check(e error, s ...string) bool {
@@ -22,6 +25,25 @@ func CheckFatal(e error, s ...string) bool {
 			log.Println(str)
 		}
 		log.Fatalln(e.Error())
+		return true
+	}
+	return false
+}
+
+// Check401 checks if an error is an error and throws a 401 Unauthorised if so
+// this is intended to be used when checking authorisation headers
+func Check401(e error, w http.ResponseWriter) bool {
+	if e != nil {
+		w.WriteHeader(http.StatusUnauthorized)
+		return true
+	}
+	return false
+}
+
+// Check500 checks if an error is an error and throws a 500 error if so
+func Check500(e error, w http.ResponseWriter) bool {
+	if e != nil {
+		w.WriteHeader(http.StatusInternalServerError)
 		return true
 	}
 	return false
